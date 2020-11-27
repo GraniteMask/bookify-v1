ed154dcea61f9775a2120691ceda1e365e08610f
Ratnadeep Das Choudhury <63235918+GraniteMask@users.noreply.github.com>
2020-11-28 01:47:18 +0530

    Bookify Final

--- backend/util.js
+++ backend/util.js
@@ -19,7 +19,7 @@ export const isAuth = (req, res, next) =>{
     const authorization = req.headers.authorization
     if(authorization){
         const token = authorization.slice(7, authorization.length)
-        jwt.verify(token, process.env.JWT_SECRET || 'sdf12s4f51s2df45sddf21gfdh54hg15hd', (err, decode)=>{
+        jwt.verify(token, process.env.JWT_SECRET , (err, decode)=>{
             if(err){
                 res.status(401).send({message: 'Invalid Token'})
             }else{
@@ -32,32 +32,6 @@ export const isAuth = (req, res, next) =>{
     }
 }
 
-// export const isAdmin = (req, res, next) =>{
-//     if(req.user && req.user.isAdmin){
-//         next()
-//     }else{
-//         res.status(401).send({message: 'Invalid Admin Token'})
-//     }
-// }
 
-// export const isAdmin = (req, res, next) =>{
-//     const authorization = req.headers.authorization
-//     if(authorization){
-//         const token = authorization.slice(7, authorization.length)
-//         jwt.verify(token, process.env.JWT_SECRET || 'sdf12s4f51s2df45sddf21gfdh54hg15hd', (err, decode)=>{
-//             if(err){
-//                 res.status(401).send({message: 'Invalid Token'})
-//             }else{
-//                 if(req.user.isAdmin){
-//                     req.user = decode
-//                     next()
-//                 }
-                 
-//             }
-//         })
-//     }else{
-//         res.status(401).send({message: 'No Token'})
-//     }
-// }
 
 //npm install dotenv
